cd745770eef9c3ede7985f0c669a7c77c58e97e8
patiako <46661598+patiako@users.noreply.github.com>
2019-01-24 09:58:17 +0800

    Add files via upload
    GetMean

---
+++ CC3 Act1/src/cc3/act1/CC3Act1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc3.act1;
+
+
+public class CC3Act1 {
+
+  
+    public static void main(String[] args) {
+        HelloWorld(); // this is a method 
+    }
+    public static void HelloWorld(){
+        
+        System.out.println("HELLO WORLD!");
+    }
+    
+} 
+    
+   
